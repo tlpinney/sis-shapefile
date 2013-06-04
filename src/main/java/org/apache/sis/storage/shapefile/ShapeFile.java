2d725fbb65f1bdd22a72957cc85ed019bd56d36b
Travis L Pinney <travis.pinney@gmail.com>
2013-06-03 21:11:02 -0400

    Add Apache license headers, small fix ups

--- src/main/java/org/apache/sis/storage/shapefile/ShapeFile.java
+++ src/main/java/org/apache/sis/storage/shapefile/ShapeFile.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.sis.storage.shapefile;
 
 import java.io.File;
@@ -65,22 +82,6 @@ public class ShapeFile {
 	public ArrayList<FieldDescriptor> FDArray = new ArrayList<FieldDescriptor>();
 	public Map<Integer, Feature> FeatureMap = new HashMap<Integer, Feature>();
 	
-	
-	
-	
-	// Helper functions to get Integers
-	
-	private int getLittleInt(byte [] data) {
-		return EndianUtils.readSwappedInteger(data, 0);
-	}
-	
-	private double getLittleDouble(byte [] data) {
-		return EndianUtils.readSwappedDouble(data, 0);
-	}
-	
-	private short getLittleShort(byte [] data) {
-		return EndianUtils.readSwappedShort(data, 0);
-	}
 	
 	
 	public ShapeFile(String shpfile) throws IOException {
@@ -236,8 +237,7 @@ public class ShapeFile {
 				int [] NumPartArr = new int[NumParts+1];
 				
 				for (int n=0; n < NumParts; n++) {
-					rf.get(data);
-					int idx = getLittleInt(data);
+					int idx = rf.getInt();
 					NumPartArr[n] = idx;
 				}
 				NumPartArr[NumParts] = NumPoints;
